b802c837791ed4271cfd5d02bbf1375c14644dfe
ReleaseBot <support@combostrap.com>
2025-11-05 18:42:35 +0000

    doc-exec v1.0.0

--- doc-exec-jre/tools/chocolateyinstall.ps1
+++ doc-exec-jre/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.21.0 at 2025-11-05T13:32:17.699251303Z
+# Generated with JReleaser 1.21.0 at 2025-11-05T18:41:46.024959225Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 $app_home = Join-Path $package 'combostrap-doc-exec-1.0.0-jre-windows-x64'
@@ -7,7 +7,7 @@ $app_exe = Join-Path $app_home 'bin/doc-exec.bat'
 Install-ChocolateyZipPackage `
     -PackageName 'doc-exec' `
     -Url 'https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-jre-windows-x64.zip' `
-    -Checksum '8390975355d811a999b7015771eb166e149a38c15bbe19b3fa57433a907d9d40' `
+    -Checksum 'dc08de48775de166261f4723442b9c150f8dcca1afdd7de094917e8f2e11c356' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
--- doc-exec-jre/tools/chocolateyuninstall.ps1
+++ doc-exec-jre/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.21.0 at 2025-11-05T13:32:17.699251303Z
+# Generated with JReleaser 1.21.0 at 2025-11-05T18:41:46.024959225Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 $app_home = Join-Path $package 'combostrap-doc-exec-1.0.0-jre-windows-x64'
