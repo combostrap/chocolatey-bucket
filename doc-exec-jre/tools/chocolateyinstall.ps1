5496b29e48736c7e0cf28e13cd3c1e61f8d84728
ReleaseBot <support@combostrap.com>
2025-11-05 12:10:34 +0000

    doc-exec v1.0.0

--- doc-exec-jre/tools/chocolateyinstall.ps1
+++ doc-exec-jre/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.21.0 at 2025-11-05T11:44:44.475262935Z
+# Generated with JReleaser 1.21.0 at 2025-11-05T12:09:24.085480989Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 $app_home = Join-Path $package 'combostrap-doc-exec-1.0.0-jre-windows-x64'
@@ -7,7 +7,7 @@ $app_exe = Join-Path $app_home 'bin/doc-exec.bat'
 Install-ChocolateyZipPackage `
     -PackageName 'doc-exec' `
     -Url 'https://github.com/combostrap/doc-exec/releases/download/v1.0.0/combostrap-doc-exec-1.0.0-jre-windows-x64.zip' `
-    -Checksum 'a76baf2e101cb2d7369921bafe4ba926cad8396af3ba45524c968c574447758a' `
+    -Checksum 'a58969f950ba1514e2e67b17638403ff4db0c4586a1d34aeff6d273c02b397cc' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
